a6eaf5db35353131e9f9d44e1f1d865901c1e725
Zaharalita Love <lovezaharalita@gmail.com>
2025-05-09 19:11:56 -0600

    Implemented local movement phase, modularity checking, and refinement phase

--- scratch_leiden.py
+++ scratch_leiden.py
@@ -9,6 +9,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random
 import copy
+from collections import defaultdict
 
 output = 'twitter_graph.tar.gz'
 extract_dir = 'twitter_graph'
@@ -95,160 +96,132 @@ print(f"success: combined graph has {G.number_of_nodes()} nodes and {G.number_of
 # communities = {node: [node] for node in nodes}
 # print("List of orig communities: ", communities)
 
-"""
-Calculates modularity gain, Check if modularity is increased or decrease when
-node is moved to new cluster(newC). Used to check if node should be moved to new cluster"""
-def modularityGain(graph, communities, node, newCommunity, initial = True):
-   
-    if communities[node] == newCommunity:
-       return -10000000
-  
-    p = [] # to test modularuty create full partition of graph including the partition above
-
-    for key in communities:# add all unique communities from original communities
-        if communities[key] not in p:
-            p.append(communities[key])
-    num = 0
-    for i in p :
-        num += len(i)
-    print("Num nodes in p: ", num,p)
-   
-    # if we are checking intial modularity before moving then skip this part
-    # if we are checking if a move increases modularity then simulate the move
-    if not initial:
-        for ls in p: # find the community with the node in it and remove node fromit
-            if node in ls:
-                ls.remove(node)   
-                print("removed: ", ls, node)
-            if ls == newCommunity: # find the community to move the node to and add node
-                ls.append(node)
-                print("added to new com: ", ls)
-
-        p = [sublist for sublist in p if sublist] # remove any empty lists
-
-        #testing to debug 
-        num = 0
-        for i in p :
-           num += len(i)
-        print("Num nodes in p: ", num)
-    print()      
-    return nx.community.modularity(graph,p)
+class Leiden():
+    def __init__(self, graph):
+        self.graph = graph
+        self.communities = {node:node for node in graph.nodes()}
 
 
-"""
-Iterates through nodes moving them to new clusters to maximize modularity.
-Performs this step some arbitrary number of times n.
-"""
-def moveNodes(graph,communities,queue,n):
-    #for range n
-    for _ in range(n):
+    def run(self):
+       self.localMoving()
 
-      random.shuffle(queue) # randomize order of nodes
+    def localMoving(self):
+        queue = list(self.graph.nodes())
+        random.shuffle(queue) # randomize order of nodes
       #for node in queue itterate through neighbors and get neighbor's community
-      while queue:
-         node = queue.pop(0)
-         community = communities[node]
-         temp = copy.deepcopy(communities)
+        while queue:
+            node = queue.pop(0)
+            community = self.communities[node]
+            bestCommunity = community
         #get initial modularity 
-         modularity = modularityGain(graph,temp,node,community)
-         betterCommunities = {} # holds neighbors whose communities increased modularity and the quality/modularity score
+            modularity = self.modularityGain(self.communities) #########
+            betterCommunities = {} # holds neighbors whose communities increased modularity and the quality/modularity score
 
          #for each neighbor check if modularity is gained when node moved to its neighbor's community
-         for neighbor in graph.neighbors(node):
-            neighborCommunity = communities[neighbor]
-            temp = copy.deepcopy(communities)
-            gain = modularityGain(graph,temp,node,neighborCommunity,False)
+            for neighbor in self.graph.neighbors(node):
+                neighborCommunity = self.communities[neighbor]
+
+                #try moving node 
+                self.communities[node] = neighborCommunity
+                gain = self.modularityGain(self.communities)
 
             #if modularity is gained then update betterCommunities
-            if gain > modularity:
-               betterCommunities[gain] = neighbor
+                if gain > modularity:
+                    betterCommunities[gain] = neighbor
+
+                #revert move 
+                self.communities[node] = community
         
         #randomly but weighted by modularity score, select a new community with improved modularity/that increases the quality function
-         if betterCommunities:
-            coms = [betterCommunities[key] for key in betterCommunities]
-            modVals = [key for key in betterCommunities]
-            #random choices doesn't evaluate if number is less than zero so convert to 0 if less than
-            for i, val in enumerate(modVals):
-               if val < 0:
-                  modVals[i] = 0
+            if betterCommunities:
+                coms = [betterCommunities[key] for key in betterCommunities]
+                modVals = [key for key in betterCommunities]
+            #random choices doesn't evaluate if number is less than or = zero so convert num if less than FIX THIS
+                for i, val in enumerate(modVals):
+                    if val < 0:
+                        modVals[i] = 0.0000000000000000001
                   
-            newCommunityNode = random.choices(coms,weights=modVals,k=1)
-            newCommunityNode = newCommunityNode[0]
+                newCommunityNode = random.choices(coms,weights=modVals,k=1)
+                newCommunityNode = newCommunityNode[0]
 
             #since node is moving to new community, add its old neighbors that were not in the new community and not in the queue to the queue
-            for neighbor in graph.neighbors(node):
-                if communities[neighbor] != communities[newCommunityNode] and neighbor not in queue:
-                   queue.append(neighbor)
+                for neighbor in self.graph.neighbors(node):
+                    if self.communities[neighbor] != self.communities[newCommunityNode] and neighbor not in queue:
+                        queue.append(neighbor)
 
-            #move node to new community 
-            communities[newCommunityNode].append(node)
+            # #move node to new community 
+            # communities[newCommunityNode].append(node)
             #set node community to new community
-            communities[node] = communities[newCommunityNode]
+                self.communities[node] = self.communities[newCommunityNode]
             
                   
-    
-    print("List of communities after local move: ",communities)
-    return communities
-         
-
-def refinementOfPartition(graph,communities):
-    refined = copy.deepcopy(communities) 
+        self.refinementOfPartition()
+      
+    def modularityGain(self,communities):
+        #dictionary of sets to prevent repeated communities in partition
+        partition = defaultdict(set)
+        #keys are set to a unique node and values are a set of nodes in their community
+        for node, com in communities.items():
+            partition[com].add(node)
+        #get list of values/communities
+        partition = list(partition.values())
+        #uses network x for calculate modularity of this partitioning
+        return nx.community.modularity(self.graph,partition)
+        
+    def refinementOfPartition(self):
+        #start with all nodes in singleton communitites
+        refined = {node: node for node in self.graph.nodes()}
+        #dictionary of sets representing comunitiy mapping of nodes from local movement phase
+        communities = defaultdict(set)
+        for node, com in self.communities.items():
+            communities[com].add(node)
 
     # get all nodes from graph and randomly shuffle their orders
-    graphNodes = []
-    for node in graph:
-        graphNodes.append(node)
-    random.shuffle(graphNodes)
+        for node in self.graph.nodes():
+            #use dict to get the nodes within the same comm and then filter to only nodes connected to current node
+            localNodes = list(communities[node])  
+            localNodes = [v for v in localNodes if v in self.graph.neighbors(node)]          
+            random.shuffle(localNodes)
 
-    # iterate through each node to find best community
-    for node in graphNodes: 
-        currentCommunity = refined[node] # node's current community
-        bestCommunity = currentCommunity 
-        nodeModularity = modularityGain(graph, refined, node, currentCommunity)  
+            currentCommunity = refined[node] # node's current community
+            bestCommunity = currentCommunity #will be set to the communitiy that maximizes modularity
+            nodeModularity = self.modularityGain(refined) #original mod
 
-        # try moving the node to each neighbor's community and see if modularity improves
-        for neighbor in graph.neighbors(node):
-            neighborCommunity = refined[neighbor]
+        # iterate through each node to find optimal move
+            for localNode in localNodes:  
 
-            # skip if the neighbor is in the same community
-            if neighborCommunity == currentCommunity:
-                continue
+                localCommunity = refined[localNode]
+                # skip if the neighbor is in the same community
+                if localCommunity == currentCommunity:
+                    continue
 
-            # copy community labels and simulate the move
-            temp = copy.deepcopy(refined)
-            neighborModularity = modularityGain(graph, temp, node, neighborCommunity, initial=False)
+                # copy community labels and simulate the move
+                refined[node] = localCommunity
+                gain = self.modularityGain(refined)
 
-            # if moving the node improves modularity, update bestCommunity
-            if neighborModularity > nodeModularity:
-                bestCommunity = neighborCommunity
-                nodeModularity = neighborModularity
-        refined[node] = bestCommunity  # assign the node to the best community found
-    
-   print("List of communities after refinement:",refined)
+                # if moving the node improves modularity, update bestCommunity
+                if gain > nodeModularity:
+                    bestCommunity = localCommunity
+                #revert move for now
+                refined[node] = currentCommunity
+            refined[node] = bestCommunity  # move the node to the best community found
+        
+        #Testing Uncomment to see refined comms
+        # r = defaultdict(set)
+        # for n, c in self.communities.items():
+        #     r[c].add(n)
+        # print(r)
+            
+    def aggregateGraph(self):
+        pass
 
-"""
- Colors the nodes according to the cluster they belong to. Returns list of node
- colors for visual
-"""
-def colorCommunities(communities,nodes):
-  pass
+    def addEdges(self):
+        pass
+    def colorCommunities(self):
+        pass
+   
 
-"""
-Returns an aggregated graph with communities represented as nodes.
-Returns dict of communities to unique nodes in graph
-"""
-def makeAggregatedGraph(communities):
-  pass
-
-"""
-Helper func for making aggregated graph, adds edges between nodes if their
- communities were connected but not the same in orig graph.
-
-Returns graph with edges
-"""
-def add_edges(aggregatedGraph):
-  pass
-
+""""Testing/Running Code"""
 
 ## Now repeat the process starting with moveNodes until modularity has been maximized
 # take subgraph of 500 nodes from largest connected component
@@ -262,6 +235,8 @@ nodes = list(G_sub.nodes())
 
 #communities start as individual nodes
 communities = {node: [node] for node in nodes}
-print("List of orig communities: ", communities)
+# print("List of orig communities: ", communities)
 
-moveNodes(G_sub,communities,nodes,5)
+alg = Leiden(G_sub)
+alg.run()
+print("Finished!")
